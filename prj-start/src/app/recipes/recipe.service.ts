f56b47fb4ea9f0416f596f1e209b6ce61a304a91
kaittiphong saetang <leng-macbook@kaittiphongs-MacBook-Pro.local>
2017-10-31 12:38:28 +0700

    reset prj-start

--- prj-start/src/app/recipes/recipe.service.ts
+++ prj-start/src/app/recipes/recipe.service.ts
@@ -27,12 +27,7 @@ export class RecipeService {
       ])
   ];
 
-  constructor(private slService: ShoppingListService) { }
-
-  setRecipes(recipes: Recipe[]) {
-    this.recipes = recipes;
-    this.recipesChanged.next(this.recipes.slice());
-  }
+  constructor(private slService: ShoppingListService) {}
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
