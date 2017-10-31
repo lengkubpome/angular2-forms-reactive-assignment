40161ea3dba0e35d621cf958222a5fd4aeb0b339
kaittiphong saetang <leng-macbook@kaittiphongs-MacBook-Pro.local>
2017-10-31 12:36:08 +0700

    ....

--- prj-start/src/app/header/header.component.ts
+++ prj-start/src/app/header/header.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { Response } from '@angular/http';
 
-<<<<<<< HEAD
 import { DataStorageService } from '../shared/data-storage.service';
-=======
-import { DataStorageService } from './../shared/data-storage.service';
->>>>>>> 8f00ed4fa7fdd31675dc524a4323f9f085f5a105
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-<<<<<<< HEAD
   constructor(private dataStorageService: DataStorageService) {}
 
   onSaveData() {
@@ -20,25 +15,11 @@ export class HeaderComponent {
       .subscribe(
         (response: Response) => {
           console.log(response);
-=======
-  constructor(private dataStorageService: DataStorageService) { }
-  
-  onSaveData() {
-    this.dataStorageService.storeRecipes()
-      .subscribe(
-        (response: Response) =>{
-          console.log(response);
-          
->>>>>>> 8f00ed4fa7fdd31675dc524a4323f9f085f5a105
         }
       );
   }
 
-<<<<<<< HEAD
   onFetchData() {
-=======
-  onFetchData(){
->>>>>>> 8f00ed4fa7fdd31675dc524a4323f9f085f5a105
     this.dataStorageService.getRecipes();
   }
 }
--- prj-start/src/app/shared/data-storage.service.ts
+++ prj-start/src/app/shared/data-storage.service.ts
@@ -5,7 +5,6 @@ import 'rxjs/Rx';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 
-<<<<<<< HEAD
 @Injectable()
 export class DataStorageService {
   constructor(private http: Http, private recipeService: RecipeService) {}
@@ -34,37 +33,3 @@ export class DataStorageService {
       );
   }
 }
-=======
-
-@Injectable()
-export class DataStorageService {
-    constructor(private http: Http,
-        private recipeService: RecipeService) { }
-
-    storeRecipes() {
-        return this.http.put('https://ng-recipe-book-af1c0.firebaseio.com/recipe.json',
-            this.recipeService.getRecipes());
-    }
-
-    getRecipes() {
-        this.http.get('https://ng-recipe-book-af1c0.firebaseio.com/recipe.json')
-            .map(
-            (response: Response) => {
-                const recipes: Recipe[] = response.json();
-                console.log(recipes);
-                for (let recipe of recipes) {     
-                    if (!recipe['ingredients']) {                        
-                        recipe['ingredients'] = [];
-                    }
-                }
-                return recipes;
-            }
-            )
-            .subscribe(
-            (recipes: Recipe[]) => {
-                this.recipeService.setRecipes(recipes);
-            }
-            );
-    }
-}
->>>>>>> 8f00ed4fa7fdd31675dc524a4323f9f085f5a105
