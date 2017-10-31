3365cd9a9f2c2d52640618d854bf5fdf1917f63d
kaittiphong saetang <leng-macbook@kaittiphongs-MacBook-Pro.local>
2017-10-31 12:40:26 +0700

    update...

--- prj-start/src/app/shared/data-storage.service.ts
+++ prj-start/src/app/shared/data-storage.service.ts
@@ -10,11 +10,11 @@ export class DataStorageService {
   constructor(private http: Http, private recipeService: RecipeService) {}
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    return this.http.put('https://ng-recipe-book-af1c0.firebaseio.com/recipes.json', this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get('https://ng-recipe-book.firebaseio.com/recipes.json')
+    this.http.get('https://ng-recipe-book-af1c0.firebaseio.com/recipes.json')
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
